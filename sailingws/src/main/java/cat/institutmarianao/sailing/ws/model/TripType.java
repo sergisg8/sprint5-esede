18da8e1d5c7253ac407c5d51fdbd2596e171288b
sergi <sergi@PortatilSergi>
2025-01-13 19:50:36 +0100

    01

--- sailingws/src/main/java/cat/institutmarianao/sailing/ws/model/TripType.java
+++ sailingws/src/main/java/cat/institutmarianao/sailing/ws/model/TripType.java
@@ -2,14 +2,13 @@ package cat.institutmarianao.sailing.ws.model;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
