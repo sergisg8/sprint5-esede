ec9865be8cfbeb5955e937ce6f88796155e4e45a
sergi <sergi@PortatilSergi>
2025-01-16 20:00:31 +0100

    amo a provar trip

--- sailingws/src/main/java/cat/institutmarianao/sailing/ws/repository/TripRepository.java
+++ sailingws/src/main/java/cat/institutmarianao/sailing/ws/repository/TripRepository.java
@@ -1,12 +1,28 @@
 package cat.institutmarianao.sailing.ws.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import cat.institutmarianao.sailing.ws.model.Trip;
+import cat.institutmarianao.sailing.ws.model.Trip.Status;
+import cat.institutmarianao.sailing.ws.model.TripType.Category;
 
 public interface TripRepository extends JpaRepository<Trip, Long> {
-	
-	@Query("SELECT t FROM Trip t WHERE "
-			+ ""
+
+	@Query("SELECT t FROM Trip t WHERE " + "(:category IS NULL OR t.departure.tripType.category = :category) AND "
+			+ "(:status IS NULL OR t.status = :status) AND "
+			+ "(:clientUsername IS NULL OR t.client.username = :clientUsername) AND "
+			+ "(:from IS NULL OR t.departure.date >= :from) AND " + "(:to IS NULL OR t.departure.date <= :to) "
+			+ "ORDER BY t.departure.date ASC")
+	List<Trip> findAllByFilters(Category category, Status status, String clientUsername, Date from, Date to);
+
+	@Query("SELECT t FROM Trip t WHERE t.client.username = :username AND "
+			+ "(:category IS NULL OR t.departure.tripType.category = :category) AND "
+			+ "(:status IS NULL OR t.status = :status) AND " + "(:from IS NULL OR t.departure.date >= :from) AND "
+			+ "(:to IS NULL OR t.departure.date <= :to) " + "ORDER BY t.departure.date ASC")
+	List<Trip> findAllByClientUsername(String username, Category category, Status status, Date from, Date to);
+
 }
--- sailingws/src/main/java/cat/institutmarianao/sailing/ws/service/impl/TripServiceImpl.java
+++ sailingws/src/main/java/cat/institutmarianao/sailing/ws/service/impl/TripServiceImpl.java
@@ -3,49 +3,98 @@ package cat.institutmarianao.sailing.ws.service.impl;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
+import cat.institutmarianao.sailing.ws.exception.ForbiddenException;
+import cat.institutmarianao.sailing.ws.exception.NotFoundException;
 import cat.institutmarianao.sailing.ws.model.Action;
 import cat.institutmarianao.sailing.ws.model.Trip;
 import cat.institutmarianao.sailing.ws.model.Trip.Status;
 import cat.institutmarianao.sailing.ws.model.TripType.Category;
+import cat.institutmarianao.sailing.ws.repository.TripRepository;
+import cat.institutmarianao.sailing.ws.security.JwtUtils;
 import cat.institutmarianao.sailing.ws.service.TripService;
+import jakarta.validation.Valid;
+import jakarta.validation.ValidationException;
 import jakarta.validation.constraints.NotNull;
 
 @Validated
 @Service
 public class TripServiceImpl implements TripService {
 
+	@Autowired
+	private TripRepository tripRepository;
+
+	@Autowired
+	private MessageSource messageSource;
+
+	@Autowired
+	private JwtUtils jwtUtils;
+
 	@Override
 	public List<Trip> findAll(Category category, Status status, String clientUsername, Date from, Date to) {
-		// TODO Auto-generated method stub
-		return null;
+		if (!jwtUtils.isAdmin()) {
+			throw new ForbiddenException(
+					messageSource.getMessage("error.Forbidden.trips.find", null, LocaleContextHolder.getLocale()));
+		}
+
+		return tripRepository.findAllByFilters(category, status, clientUsername, from, to);
 	}
 
 	@Override
 	public List<Trip> findAllByClientUsername(@NotNull String username, Category category, Status status, Date from,
 			Date to) {
-		// TODO Auto-generated method stub
-		return null;
+		if (!(jwtUtils.isAdmin() || jwtUtils.isAuthUser(username))) {
+			throw new ForbiddenException(
+					messageSource.getMessage("error.Forbidden.trips.find", null, LocaleContextHolder.getLocale()));
+		}
+
+		return tripRepository.findAllByFilters(category, status, username, from, to);
 	}
 
 	@Override
 	public Trip findById(@NotNull Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		if (!jwtUtils.isAdmin()) {
+			throw new ForbiddenException(
+					messageSource.getMessage("error.Forbidden.trips.get", null, LocaleContextHolder.getLocale()));
+		}
+
+		return tripRepository.findById(id)
+				.orElseThrow(() -> new NotFoundException(messageSource.getMessage("error.NotFound.resource.by.id",
+						new String[] { "Trip", id.toString() }, LocaleContextHolder.getLocale())));
 	}
 
 	@Override
-	public Trip save(@NotNull Trip trip) {
-		// TODO Auto-generated method stub
-		return null;
+	public Trip save(@NotNull @Valid Trip trip) {
+		if (!jwtUtils.isAdmin()) {
+			throw new ForbiddenException(
+					messageSource.getMessage("error.Forbidden.trips.create", null, LocaleContextHolder.getLocale()));
+		}
+
+		if (tripRepository.existsById(trip.getId())) {
+			throw new ValidationException(messageSource.getMessage("error.TripService.id.exists",
+					new String[] { trip.getId().toString() }, LocaleContextHolder.getLocale()));
+		}
+
+		return tripRepository.saveAndFlush(trip);
 	}
 
 	@Override
 	public Action saveAction(Action action) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		if (!(jwtUtils.isAdmin() || jwtUtils.isAuthUser(action.getTrip().getClient().getUsername()))) {
+			throw new ForbiddenException(
+					messageSource.getMessage("error.Forbidden.actions.create", null, LocaleContextHolder.getLocale()));
+		}
 
+		if (!tripRepository.existsById(action.getTrip().getId())) {
+			throw new NotFoundException(messageSource.getMessage("error.NotFound.resource.by.id",
+					new String[] { "Trip", action.getTrip().getId().toString() }, LocaleContextHolder.getLocale()));
+		}
+		return null;
+		// return tripRepository.saveAction(action);
+	}
 }
